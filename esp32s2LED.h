ebd661db84736e9edd5338e497e6aec50167452a
blekenbleu <blekenbleu@gmail.com>
2021-11-11 16:19:07 -0500

    add usage comment

--- esp32s2LED.h
+++ esp32s2LED.h
@@ -1,9 +1,29 @@
+// a wrapper for Freenove_WS2812_Lib_for_ESP32.h
 #include "Freenove_WS2812_Lib_for_ESP32.h"
+
+/* typical usage
+ ; #include "esp32s2LED.h
+ ;
+ ; CREATE_ESP32_WS2812_INSTANCE();
+ ;
+ ; void setup() {
+ ;   ...
+ ;   ESP32_WS2812_SETUP(255);
+ ;   ...
+ ; }
+ ;
+ ; void loop() {
+ ;   ESP32_RED(0);
+ ;   ...
+ ;   ...
+ ;   ESP32_LED(0, 255, 0);  // green
+ ; }
+ */
 
 #define LED_PIN  18  // GPIO18 for ESP32-S2-Saola-1
 
 // ESP32-S2-Saola-1 has only a single WS2812
 #define CREATE_ESP32_WS2812_INSTANCE() Freenove_ESP32_WS2812 ws2812 = Freenove_ESP32_WS2812(1, LED_PIN, 0, TYPE_GRB)
 #define ESP32_RED(val) ws2812.setLedColorData(0, val, 0, 0); ws2812.show()
-#define ESP32_RED(r, g, b) ws2812.setLedColorData(0, r, g, b); ws2812.show()
+#define ESP32_LED(r, g, b) ws2812.setLedColorData(0, r, g, b); ws2812.show()
 #define ESP32_WS2812_SETUP(val) ws2812.begin(); ws2812.setBrightness(10); ESP32_RED(val)
