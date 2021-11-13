4004ca87309b23f62d6c38092a2664ec5ba6815f
blekenbleu <blekenbleu@gmail.com>
2021-11-13 08:42:22 -0500

    legal multi-line macros

--- esp32s2LED.h
+++ esp32s2LED.h
@@ -20,10 +20,13 @@
  ; }
  */
 
-#define LED_PIN  18  // GPIO18 for ESP32-S2-Saola-1
+#define WS2812_PIN  18  // GPIO18 for ESP32-S2-Saola-1M (WROOM)
 
-// ESP32-S2-Saola-1 has only a single WS2812
-#define CREATE_ESP32_WS2812_INSTANCE() Freenove_ESP32_WS2812 ws2812 = Freenove_ESP32_WS2812(1, LED_PIN, 0, TYPE_GRB)
-#define ESP32_RED(val) ws2812.setLedColorData(0, val, 0, 0); ws2812.show()
-#define ESP32_LED(r, g, b) ws2812.setLedColorData(0, r, g, b); ws2812.show()
-#define ESP32_WS2812_SETUP(val) ws2812.begin(); ws2812.setBrightness(10); ESP32_RED(val)
+// ESP32-S2-Saola-1M has only a single WS2812
+#define CREATE_ESP32_WS2812_INSTANCE() Freenove_ESP32_WS2812 ws2812 = Freenove_ESP32_WS2812(1, WS2812_PIN, 0, TYPE_GRB)
+
+#define ESP32_WS2812_SETUP(val) ({ws2812.begin(); ws2812.setBrightness(10); ESP32_RED(val);})
+
+#define ESP32_RED(val) ({ws2812.setLedColorData(0, val, 0, 0); ws2812.show();})
+
+#define ESP32_LED(r, g, b) ({ws2812.setLedColorData(0, r, g, b); ws2812.show();})
