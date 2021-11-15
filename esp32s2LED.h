e62f7a89fcc76479a69bf85d0b97722bd6ed4dfe
blekenbleu <blekenbleu@gmail.com>
2021-11-14 21:07:01 -0500

    white space; remove USBCON

--- esp32s2LED.h
+++ esp32s2LED.h
@@ -1,6 +1,5 @@
 // a wrapper for Freenove_WS2812_Lib_for_ESP32.h
 #include "Freenove_WS2812_Lib_for_ESP32.h"
-#define USBCON 1		// for MIDIUSB
 
 /* typical usage
  ; #include "esp32s2LED.h
@@ -24,10 +23,18 @@
 #define WS2812_PIN  18  // GPIO18 for ESP32-S2-Saola-1M (WROOM)
 
 // ESP32-S2-Saola-1M has only a single WS2812
-#define CREATE_ESP32_WS2812_INSTANCE() Freenove_ESP32_WS2812 ws2812 = Freenove_ESP32_WS2812(1, WS2812_PIN, 0, TYPE_GRB)
+#define CREATE_ESP32_WS2812_INSTANCE() Freenove_ESP32_WS2812 ws2812 \
+        = Freenove_ESP32_WS2812(1, WS2812_PIN, 0, TYPE_GRB)
 
-#define ESP32_WS2812_SETUP(val) ({ws2812.begin(); ws2812.setBrightness(10); ESP32_RED(val);})
+#define ESP32_WS2812_SETUP(val) ({ \
+        ws2812.begin(); \
+        ws2812.setBrightness(10); \
+        ESP32_RED(val); })
 
-#define ESP32_RED(val) ({ws2812.setLedColorData(0, val, 0, 0); ws2812.show();})
+#define ESP32_RED(val) ({ \
+        ws2812.setLedColorData(0, val, 0, 0); \
+        ws2812.show(); })
 
-#define ESP32_LED(r, g, b) ({ws2812.setLedColorData(0, r, g, b); ws2812.show();})
+#define ESP32_LED(r, g, b) ({ \
+        ws2812.setLedColorData(0, r, g, b); \
+        ws2812.show(); })
